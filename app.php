2c4b057ca74962d5974fd91521a29f492dc4710e
Mitchell Hislop <mitch@mitchellhislop.com>
2011-03-26 14:45:57 -0500

    first version complete

--- app.php
+++ app.php
@@ -1,2 +1,29 @@
+<?php
+
 include 'twilio-php/twilio.php';
 include 'config.php';
+
+$client = new TwilioRestClient($AccountSid, $AuthToken);
+//this will get pulled out of the database of people who have signed up for SMS
+$testers=array(
+	"6124184992" => "Mitchell",
+	"6127203122" => "Justin"
+	);
+	
+	
+//build this for new posts, tweets, whatever we want	
+foreach ($testers as $number => $name){
+	$response = $client->request("/$ApiVersion/Accounts/$AccountSid/SMS/Messages", 
+	            "POST", array(
+	            "To" => $number,
+	            "From" => "612-360-2696",
+	            "Body" => "Hello $name, you look nice today"
+	        ));
+
+		if($response->IsError)
+            echo "Error: {$response->ErrorMessage}";
+        else
+            echo "Sent message to $name";
+    }
+ 
+?>
