7e155bd671f4cf54ecc406a48eee92422311fc0b
Mitchell Hislop <mitch@mitchellhislop.com>
2011-03-26 20:39:47 -0500

    adding in the interactive. Cases are done, need to add in the DB interaction

--- response.php
+++ response.php
@@ -1,17 +1,28 @@
 <?php
 $from=$_REQUEST['From'];
 $case = $_REQUEST['Body'];
-
-if ($case == "add")
+$case=strtoupper($case);
+if ($case == "ADD")
 {
-	$message="You have been added. Welcome";
+	$message="You have been added. Thank you for supporting Project 515";
+	//add to the db
 }
-else if ($case == "stop")
+else if ($case == "STOP")
 {
-	$message = "Ok. I will stop";
+	$message = "You have been unsubscribed. Txt Add to subscribe again";
+	//switch active to 0
+}
+else if($case=="NEWS"){
+	//pull recent news headline
+	$message = "The most recent news item is: TITLE LINK";
+}
+else if ($case=="EVENT" OR $case=="EVENTS")
+{
+	//pull most recent event
+	$message="The next event is TITLE on DATE at LOCATION";
 }
 else {
-	$message = "I didnt understand. Text add to add, or stop to stop";
+	$message="I didn't understand that. Try Add, News, Event, or Stop"
 }
  header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
